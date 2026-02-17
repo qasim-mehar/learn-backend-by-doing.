b540e79c434ef73e9e4f8583c16793b620443e93
Qasim <mianqasim065@gmail.com>
2026-02-17 20:16:04 +0500

    feat: add POST /notes endpoint to create a new note

--- notes_manager/src/app.js
+++ notes_manager/src/app.js
@@ -6,7 +6,7 @@ const notes = [];
 app.post("/notes", (req, res) => {
   notes.push(req.body);
   res.status(201).json({
-    message: "Note created succefully!",
+    message: "Note created succefully",
   });
 });
 
