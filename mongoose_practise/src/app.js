f7f7082dcbcc916ac94ee4eca964a6df3b5190d2
Qasim <mianqasim065@gmail.com>
2026-02-22 12:14:26 +0500

    feat: add GET /notes route to fetch all notes

--- mongoose_practise/src/app.js
+++ mongoose_practise/src/app.js
@@ -15,4 +15,14 @@ app.post("/notes", async (req, res) => {
   });
 });
 
+app.get("/notes", async (req, res) => {
+  //find() method return an array of all data or no data,
+  //However, findOne({condition}) method a single note match with given condition if condition not match it will return null not array!
+  const notes = await noteModel.find();
+  res.status(200).json({
+    message: "Notes fetched success fully",
+    notes,
+  });
+});
+
 module.exports = app;
