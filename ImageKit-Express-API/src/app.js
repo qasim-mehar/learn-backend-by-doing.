2c129c846bdaaae328aa01e82c609469c4762cb4
Qasim <mianqasim065@gmail.com>
2026-02-25 20:31:58 +0500

    feat(api): initialize express app with multer for file uploads

--- ImageKit-Express-API/src/app.js
+++ ImageKit-Express-API/src/app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const multer = require("multer");
 const app = express();
 app.use(express.json());
 
+const upload = multer({ storage: multer.memoryStorage() });
+// app.post("/create-post", async (req,res)={
+
+// })
 module.exports = app;
