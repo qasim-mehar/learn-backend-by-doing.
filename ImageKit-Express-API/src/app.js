5c6f2462a80f93fc48bab8ce997d2c7441547465
Qasim <mianqasim065@gmail.com>
2026-02-26 20:08:19 +0500

    feat(api): implement post creation endpoint with image upload

--- ImageKit-Express-API/src/app.js
+++ ImageKit-Express-API/src/app.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const multer = require("multer");
+const uploadFile = require("./services/storage.service");
+const postModel = require("./models/post.model");
 const app = express();
 app.use(express.json());
 
 const upload = multer({ storage: multer.memoryStorage() });
-// app.post("/create-post", async (req,res)={
 
-// })
+//"image" in single method is the name of filename field in postman to determine file
+app.post("/create-post", upload.single("image"), async (req, res) => {
+  const result = await uploadFile(req.file.buffer);
+  const post = await postModel.create({
+    image: result.url,
+    caption: req.body.caption,
+  });
+  return res.status(201).json({
+    message: "Post created successfully",
+    post,
+  });
+});
 module.exports = app;
