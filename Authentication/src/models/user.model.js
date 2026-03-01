d6bb3a68f7f5d9d514db16d411e9bba06a792732
Qasim <mianqasim065@gmail.com>
2026-03-01 20:32:40 +0500

    fix: user can only register with a unique email address

--- Authentication/src/models/user.model.js
+++ Authentication/src/models/user.model.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   username: String,
   password: String,
-  email: String,
+  email: {
+    type: String,
+    unique: true,
+  },
 });
 
 const userModel = mongoose.model("user", userSchema);
